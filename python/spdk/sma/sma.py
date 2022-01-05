e4b75e8da94f879c004530be9c051135cbd1dc66
Konrad Sztyber <konrad.sztyber@intel.com>
2022-01-05 10:32:09 +0100

    sma: initial Storage Management Agent structures
    Storage Management Agent is a gRPC server that provides an abstraction
    layer above the SPDK RPC interface.  The interface aims to provide a set
    of methods for managing various protocols (e.g. NVMe, virtio-blk) while
    hiding the details of a particular transport.
    
    The external API is defined by `lib/python/spdk/sma/proto/sma.proto`
    protobuf file.  It defines the generic gRPC service methods.  The
    subsystem-specific parameters are wrapped by a google.protobuf.Any
    object and are defined by a subsystem-specific protobuf file.
    
    This patch implements a gRPC service that delegates the work to a specific
    subsystem.  A subsystem is a class that implements some of the methods
    defined by the service for a given type of devices (e.g. NVMe/TCP,
    NVMe/PCIe, virtio-blk, etc.).  For now, only the RPC for creating a
    device is implemented, others are added in subsequent patches.
    
    The series implements the generic calls as well as their NVMe/TCP
    implementation.  Support for other devce types could be easily added by
    creating a new subsystem and defining its protobuf parameter definition.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I17cde3b31d3514878f1027cfcd112b48848f6123

---
+++ python/spdk/sma/sma.py
@@ -0,0 +1,65 @@
+from concurrent import futures
+from contextlib import contextmanager
+from multiprocessing import Lock
+import grpc
+import logging
+from .subsystem import subsystem
+from .proto import sma_pb2 as pb2
+from .proto import sma_pb2_grpc as pb2_grpc
+
+
+class UnsupportedSubsystemException(Exception):
+    pass
+
+
+class StorageManagementAgent(pb2_grpc.StorageManagementAgentServicer):
+    def __init__(self, addr, port):
+        self._subsystems = {}
+        self._server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        self._lock = Lock()
+        self._server.add_insecure_port(f'{addr}:{port}')
+        pb2_grpc.add_StorageManagementAgentServicer_to_server(self, self._server)
+
+    def _grpc_method(f):
+        def wrapper(self, request, context):
+            logging.debug(f'{f.__name__}\n{request}')
+            # For now, synchronize all gRPC methods
+            self._lock.acquire()
+            try:
+                return f(self, request, context)
+            finally:
+                self._lock.release()
+        return wrapper
+
+    def register_subsystem(self, subsystem):
+        self._subsystems[subsystem.name] = subsystem
+
+    def run(self):
+        self._server.start()
+        self._server.wait_for_termination()
+
+    def _find_subsystem(self, name):
+        subsys = self._subsystems.get(name)
+        if subsys is None:
+            raise UnsupportedSubsystemException()
+        return subsys
+
+    @_grpc_method
+    def CreateDevice(self, request, context):
+        response = pb2.CreateDeviceResponse()
+        try:
+            if not request.HasField('type'):
+                raise subsystem.SubsystemException(grpc.StatusCode.INVALID_ARGUMENT,
+                                                   'Missing required field: type')
+            subsys = self._find_subsystem(request.type.value)
+            response = subsys.create_device(request)
+        except UnsupportedSubsystemException:
+            context.set_details('Invalid device type')
+            context.set_code(grpc.StatusCode.INVALID_ARGUMENT)
+        except subsystem.SubsystemException as ex:
+            context.set_details(ex.message)
+            context.set_code(ex.code)
+        except NotImplementedError:
+            context.set_details('Method is not implemented by selected device type')
+            context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        return response
---
+++ python/spdk/sma/subsystem/subsystem.py
@@ -0,0 +1,32 @@
+import multiprocessing
+from ..proto import sma_pb2
+
+
+class SubsystemException(Exception):
+    def __init__(self, code, message):
+        self.code = code
+        self.message = message
+
+
+class Subsystem:
+    def __init__(self, name, client):
+        self._client = client
+        self.name = name
+
+    def init(self, config):
+        pass
+
+    def create_device(self, request):
+        raise NotImplementedError()
+
+    def remove_device(self, request):
+        raise NotImplementedError()
+
+    def attach_volume(self, request):
+        raise NotImplementedError()
+
+    def detach_volume(self, request):
+        raise NotImplementedError()
+
+    def owns_device(self, id):
+        raise NotImplementedError()
