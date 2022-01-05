a8203d2a329785c21ef17c6287c15e81617497bd
Konrad Sztyber <konrad.sztyber@intel.com>
2022-01-05 10:32:09 +0100

    [RFC] sma: initial Storage Management Agent structures
    Storage Management Agent is a gRPC server that provides an abstraction
    layer above the SPDK RPC interface.  Its methods are simplified and aim
    to provide a consistent APIs for different storage device models (NVMe,
    virtio-blk, virtio-scsi).
    
    The external API is defined by `lib/python/spdk/sma/proto/sma.proto`
    protobuf file.  It defines the generic gRPC service methods.  The
    subsystem-specific parameters are wrapped by a google.protobuf.Any
    object and are defined by a subsystem-specific protobuf file.
    
    This patch implements a gRPC service that delegates the work to a specific
    subsystem.  A subsystem is a class that implements some of the methods
    defined by the service for a given type of devices (e.g. NVMe/TCP,
    NVMe/PCIe, virtio-blk, etc.).  For now, only the RPC for creating a
    device is implemented, others are added in subsequent patches.
    
    The series implements the generic calls as well as their NVMe/TCP
    implementation.  Support for other device types could be easily added by
    creating a new subsystem and defining its protobuf parameter definition.
    Not all device types need to implement all RPCs, for instance NVMe/PCIe
    doesn't need to implement (Create|Remove)Device methods.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I17cde3b31d3514878f1027cfcd112b48848f6123

---
+++ lib/python/spdk/sma/sma.py
@@ -0,0 +1,72 @@
+from concurrent import futures
+from contextlib import contextmanager
+from multiprocessing import Lock
+import grpc
+import logging
+from .subsystem import subsystem
+from .proto import sma_pb2 as pb2
+from .proto import sma_pb2_grpc as pb2_grpc
+
+
+class UnsupportedSubsystemException(Exception):
+    pass
+
+
+class StorageManagementAgent(pb2_grpc.StorageManagementAgentServicer):
+    _lock = Lock()
+
+    def __init__(self, client, addr, port):
+        self._subsystems = {}
+        self._server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        self._client = lambda: self._get_client(client)
+        self._server.add_insecure_port(f'{addr}:{port}')
+        pb2_grpc.add_StorageManagementAgentServicer_to_server(self, self._server)
+
+    @contextmanager
+    def _get_client(self, client):
+        # For now, use this to synchronize multiple RPC calls via the with client block
+        self._lock.acquire()
+        try:
+            yield client()
+        finally:
+            self._lock.release()
+
+    def _log_method(f):
+        def wrapper(self, request, context):
+            logging.debug(f'{f.__name__}\n{request}')
+            return f(self, request, context)
+        return wrapper
+
+    def register_subsystem(self, subsys_cls):
+        subsys = subsys_cls(self._client)
+        self._subsystems[subsys.name] = subsys
+
+    def run(self):
+        self._server.start()
+        self._server.wait_for_termination()
+
+    def _find_subsystem(self, name):
+        subsys = self._subsystems.get(name)
+        if subsys is None:
+            raise UnsupportedSubsystemException()
+        return subsys
+
+    @_log_method
+    def CreateDevice(self, request, context):
+        response = pb2.CreateDeviceResponse()
+        try:
+            if not request.HasField('type'):
+                raise subsystem.SubsystemException(grpc.StatusCode.INVALID_ARGUMENT,
+                                                   'Missing required field: type')
+            subsys = self._find_subsystem(request.type.value)
+            response = subsys.create_device(request)
+        except UnsupportedSubsystemException:
+            context.set_details('Invalid device type')
+            context.set_code(grpc.StatusCode.INTERNAL)
+        except subsystem.SubsystemException as ex:
+            context.set_details(ex.message)
+            context.set_code(ex.code)
+        except NotImplementedError:
+            context.set_details('Method is not implemented by selected device type')
+            context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        return response
