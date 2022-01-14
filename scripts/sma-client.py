68f99f951a5b5e6ee4582fd8e0510c2ccdcde9cd
mionsz <milosz.linkiewicz@intel.com>
2022-01-14 20:12:07 +0000

    [RFC] sma: Enable SMA TLS authentication
    --cert-chain - a list of certificates used to authenticate an entity
    --priv-key - private key generated with certificate
    --root-cert - certificate issued by a trusted certificate authority
    
    Change-Id: I1891a39a6f95ad83427c26393a97b51173d4d08f
    Signed-off-by: Sebastian Brzezinka <sebastian.brzezinka@intel.com>

--- scripts/sma-client.py
+++ scripts/sma-client.py
@@ -16,10 +16,15 @@ import spdk.sma.proto.nvmf_tcp_pb2_grpc as nvmf_tcp_pb2_grpc    # noqa
 
 
 class Client:
-    def __init__(self, addr, port):
+    def __init__(self, addr, port, root_cert):
         self._service = sma_pb2.DESCRIPTOR.services_by_name['StorageManagementAgent']
         self.addr = addr
         self.port = port
+        if root_cert is not None:
+            with open(root_cert, 'rb') as f:
+                self.creds = grpc.ssl_channel_credentials(f.read())
+        else:
+            self.creds = None
 
     def _get_message_type(self, descriptor):
         return getattr(sma_pb2, descriptor.name)
@@ -29,14 +34,19 @@ class Client:
         return (self._get_message_type(method.input_type),
                 self._get_message_type(method.output_type))
 
+    def _get_channel(self):
+        if self.creds is not None:
+            return grpc.secure_channel(f'{self.addr}:{self.port}', creds)
+        else:
+            return grpc.insecure_channel(f'{self.addr}:{self.port}')
+
     def call(self, method, params):
-        with grpc.insecure_channel(f'{self.addr}:{self.port}') as channel:
-            stub = sma_pb2_grpc.StorageManagementAgentStub(channel)
-            func = getattr(stub, method)
-            input, output = self._get_method_types(method)
-            response = func(request=json_format.ParseDict(params, input()))
-            return json_format.MessageToDict(response,
-                                             preserving_proto_field_name=True)
+        stub = sma_pb2_grpc.StorageManagementAgentStub(self._get_channel())
+        func = getattr(stub, method)
+        input, output = self._get_method_types(method)
+        response = func(request=json_format.ParseDict(params, input()))
+        return json_format.MessageToDict(response,
+                                         preserving_proto_field_name=True)
 
 
 def parse_argv():
@@ -45,12 +55,14 @@ def parse_argv():
                         help='IP address of SMA instance to connect to')
     parser.add_argument('--port', '-p', default=50051, type=int,
                         help='Port number of SMA instance to connect to')
+    parser.add_argument('--root-cert', '-r',
+                        help='The PEM-encoded root certificates as a byte string')
     return parser.parse_args()
 
 
 def main(args):
     argv = parse_argv()
-    client = Client(argv.address, argv.port)
+    client = Client(argv.address, argv.port, argv.root_cert)
     request = json.loads(sys.stdin.read())
     result = client.call(request['method'], request.get('params', {}))
     print(json.dumps(result, indent=2))
