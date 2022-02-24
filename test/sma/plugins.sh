14720de3a82dbdaeb217526aff939d992327f454
Konrad Sztyber <konrad.sztyber@intel.com>
2022-02-24 13:17:31 +0100

    test/sma: plugin test
    The test verifies that it's possible to register submodules from
    out-of-tree plugins.  The test defines two plugins, each defining two
    subystems implementing the same two protocols and verifies that it's
    possible to register different combinations.
    
    Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
    Change-Id: I2144b40db603fea95bf8b571777e6662b4de9bc7

---
+++ test/sma/plugins.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+testdir=$(readlink -f "$(dirname "$0")")
+rootdir=$(readlink -f "$testdir/../..")
+
+source "$rootdir/test/common/autotest_common.sh"
+source "$testdir/common.sh"
+
+function create_device() {
+	"$rootdir/scripts/sma-client.py" <<- EOF
+		{
+			"method": "CreateDevice",
+			"params": {
+				"type": "$1"
+			}
+		}
+	EOF
+}
+
+trap 'killprocess $smapid; exit 1' SIGINT SIGTERM EXIT
+
+# First check a single plugin with both its subsystems enabled in the config
+PYTHONPATH=$testdir/plugins $rootdir/scripts/sma.py -c <(
+	cat <<- EOF
+		plugins:
+		  - 'plugin1'
+		subsystems:
+		  - name: 'plugin1-subsys1'
+		  - name: 'plugin1-subsys2'
+	EOF
+) &
+smapid=$!
+# Wait for a while to make sure SMA starts listening
+sma_waitforlisten
+
+[[ $(create_device protocol1 | jq -r '.id') == 'protocol1:plugin1-subsys1' ]]
+[[ $(create_device protocol2 | jq -r '.id') == 'protocol2:plugin1-subsys2' ]]
+
+killprocess $smapid
+
+# Check that it's possible to enable only a single subsystem from a plugin
+PYTHONPATH=$testdir/plugins $rootdir/scripts/sma.py -c <(
+	cat <<- EOF
+		plugins:
+		  - 'plugin1'
+		subsystems:
+		  - name: 'plugin1-subsys2'
+	EOF
+) &
+smapid=$!
+sma_waitforlisten
+
+[[ $(create_device protocol2 | jq -r '.id') == 'protocol2:plugin1-subsys2' ]]
+NOT create_device protocol1
+
+killprocess $smapid
+
+# Load two different plugins, but only enable subsystems from one of them
+PYTHONPATH=$testdir/plugins $rootdir/scripts/sma.py -c <(
+	cat <<- EOF
+		plugins:
+		  - 'plugin1'
+		  - 'plugin2'
+		subsystems:
+		  - name: 'plugin1-subsys1'
+		  - name: 'plugin1-subsys2'
+	EOF
+) &
+smapid=$!
+sma_waitforlisten
+
+[[ $(create_device protocol1 | jq -r '.id') == 'protocol1:plugin1-subsys1' ]]
+[[ $(create_device protocol2 | jq -r '.id') == 'protocol2:plugin1-subsys2' ]]
+
+killprocess $smapid
+
+# Check the same but take subsystems defined by the other plugin
+PYTHONPATH=$testdir/plugins $rootdir/scripts/sma.py -c <(
+	cat <<- EOF
+		plugins:
+		  - 'plugin1'
+		  - 'plugin2'
+		subsystems:
+		  - name: 'plugin2-subsys1'
+		  - name: 'plugin2-subsys2'
+	EOF
+) &
+smapid=$!
+sma_waitforlisten
+
+[[ $(create_device protocol1 | jq -r '.id') == 'protocol1:plugin2-subsys1' ]]
+[[ $(create_device protocol2 | jq -r '.id') == 'protocol2:plugin2-subsys2' ]]
+
+killprocess $smapid
+
+# Now pick a subsystem from each plugin
+PYTHONPATH=$testdir/plugins $rootdir/scripts/sma.py -c <(
+	cat <<- EOF
+		plugins:
+		  - 'plugin1'
+		  - 'plugin2'
+		subsystems:
+		  - name: 'plugin1-subsys1'
+		  - name: 'plugin2-subsys2'
+	EOF
+) &
+smapid=$!
+sma_waitforlisten
+
+[[ $(create_device protocol1 | jq -r '.id') == 'protocol1:plugin1-subsys1' ]]
+[[ $(create_device protocol2 | jq -r '.id') == 'protocol2:plugin2-subsys2' ]]
+
+killprocess $smapid
+
+# Check the same, but register plugins via a env var
+PYTHONPATH=$testdir/plugins SMA_PLUGINS=plugin1:plugin2 $rootdir/scripts/sma.py -c <(
+	cat <<- EOF
+		subsystems:
+		  - name: 'plugin1-subsys1'
+		  - name: 'plugin2-subsys2'
+	EOF
+) &
+smapid=$!
+sma_waitforlisten
+
+[[ $(create_device protocol1 | jq -r '.id') == 'protocol1:plugin1-subsys1' ]]
+[[ $(create_device protocol2 | jq -r '.id') == 'protocol2:plugin2-subsys2' ]]
+
+killprocess $smapid
+
+# Finally, register one plugin in a config and the other through env var
+PYTHONPATH=$testdir/plugins SMA_PLUGINS=plugin1 $rootdir/scripts/sma.py -c <(
+	cat <<- EOF
+		plugins:
+		  - 'plugin2'
+		subsystems:
+		  - name: 'plugin1-subsys1'
+		  - name: 'plugin2-subsys2'
+	EOF
+) &
+smapid=$!
+sma_waitforlisten
+
+[[ $(create_device protocol1 | jq -r '.id') == 'protocol1:plugin1-subsys1' ]]
+[[ $(create_device protocol2 | jq -r '.id') == 'protocol2:plugin2-subsys2' ]]
+
+killprocess $smapid
+
+trap - SIGINT SIGTERM EXIT
---
+++ test/sma/plugins/plugin2/__init__.py
@@ -0,0 +1,24 @@
+from google.protobuf import wrappers_pb2 as wrap
+from spdk.sma import subsystem
+from spdk.sma.proto import sma_pb2
+
+
+class TestSubsystem1(subsystem.Subsystem):
+    def __init__(self, client):
+        super().__init__('plugin2-subsys1', 'protocol1', client)
+
+    def create_device(self, request):
+        return sma_pb2.CreateDeviceResponse(id=wrap.StringValue(
+                    value=f'{self.protocol}:{self.name}'))
+
+
+class TestSubsystem2(subsystem.Subsystem):
+    def __init__(self, client):
+        super().__init__('plugin2-subsys2', 'protocol2', client)
+
+    def create_device(self, request):
+        return sma_pb2.CreateDeviceResponse(id=wrap.StringValue(
+                    value=f'{self.protocol}:{self.name}'))
+
+
+subsystems = [TestSubsystem1, TestSubsystem2]
